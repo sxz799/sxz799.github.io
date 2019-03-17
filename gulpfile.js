c113cea5b8f94bd0a7b130984a958e6c00c2d7fc
sxz799 <songxianzhi521@qq.com>
2019-03-17 22:59:20 +0800

    Site updated: 2019-03-17 22:59:20

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,47 @@ var minifycss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
-// 压缩 public 目录 css
+var imagemin = require('gulp-imagemin');
+ 
+// 压缩html
+gulp.task('minify-html', function() {
+    return gulp.src('./public/**/*.html')
+        .pipe(htmlclean())
+        .pipe(htmlmin({
+            removeComments: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
+        }))
+        .pipe(gulp.dest('./public'))
+});
+// 压缩css
 gulp.task('minify-css', function() {
     return gulp.src('./public/**/*.css')
-        .pipe(minifycss())
+        .pipe(minifycss({
+            compatibility: 'ie8'
+        }))
         .pipe(gulp.dest('./public'));
 });
-// 压缩 public 目录 html
-gulp.task('minify-html', function() {
-  return gulp.src('./public/**/*.html')
-    .pipe(htmlclean())
-    .pipe(htmlmin({
-         removeComments: true,
-         minifyJS: true,
-         minifyCSS: true,
-         minifyURLs: true,
-    }))
-    .pipe(gulp.dest('./public'))
-});
-// 压缩 public/js 目录 js
+// 压缩js
 gulp.task('minify-js', function() {
     return gulp.src('./public/js/**/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('./public'));
 });
+// 压缩图片
+gulp.task('minify-images', function() {
+    return gulp.src('./public/images/**/*.*')
+        .pipe(imagemin(
+        [imagemin.gifsicle({'optimizationLevel': 3}), 
+        imagemin.jpegtran({'progressive': true}), 
+        imagemin.optipng({'optimizationLevel': 7}), 
+        imagemin.svgo()],
+        {'verbose': true}))
+        .pipe(gulp.dest('./public/images'))
+});
+// 默认任务
+// gulp 4.0 适用的方式
+gulp.task('build', gulp.parallel('minify-html', 'minify-css', 'minify-js'
+ //build the website
+));
